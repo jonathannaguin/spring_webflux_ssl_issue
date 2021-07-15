2b58f6b8852003867a2fd1039b73fe03b13c3732
Jonathan Nunez Aguin <JonathanNuNezAguin@fico.com>
2021-07-15 21:48:05 +0200

    Removing swagger.

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,10 +1,5 @@
 package com.example.demo;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,11 +21,6 @@ public class Controller
     private static final Logger logger = LoggerFactory.getLogger(Controller.class);
     private Service service;
 
-    @Operation(description = "Request")
-    @ApiResponses({ @ApiResponse(responseCode = "200", description = "Message processed correctly",
-                                 content = @Content(schema = @Schema(implementation = Response.class))),
-                    @ApiResponse(responseCode = "400", description = "Validation failed",
-                                 content = @Content(schema = @Schema(implementation = Error.class))) })
     @PostMapping(value = { "/request" }, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Mono<Response> request(@Valid @RequestBody Request request)
     {
